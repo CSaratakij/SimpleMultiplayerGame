fba656b1d22227d0374039609061f1fc4a6db2fa
Chatchai Saratakij <CSaratakij@gmail.com>
2018-07-30 00:12:25 +0700

    Add placeholder ui

---
+++ Assets/Scripts/UI/UI/GameUIController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MVP.UI
+{
+    public class GameUIController : MonoBehaviour
+    {
+        public enum View
+        {
+            MainMenu,
+            CreateGame
+        }
+
+        public static GameUIController instance = null;
+
+        [SerializeField]
+        RectTransform[] menu;
+
+        void Awake()
+        {
+            if (instance == null) {
+                instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else {
+                Destroy(gameObject);
+            }
+        }
+
+        void Start()
+        {
+            HideAllMenu();
+            Show(View.MainMenu);
+        }
+
+        public void Show(View view) {
+            menu[(int)view].gameObject.SetActive(true);
+        }
+
+        public void HideAllMenu()
+        {
+            foreach (RectTransform rect in menu) {
+                rect.gameObject.SetActive(false);
+            }
+        }
+    }
+}
