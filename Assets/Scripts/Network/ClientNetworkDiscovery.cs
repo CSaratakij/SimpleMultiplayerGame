4a677be0a54d796ad23c60fa3ef34fb46294e234
Chatchai Saratakij <CSaratakij@gmail.com>
2018-07-30 21:41:46 +0700

    Make player able to find a lan server

---
+++ Assets/Scripts/Network/ClientNetworkDiscovery.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace MVP
+{
+    public class ClientNetworkDiscovery : NetworkDiscovery
+    {
+        public override void OnReceivedBroadcast(string fromAddress, string data)
+        {
+            NetworkManager.singleton.networkAddress = fromAddress;
+            NetworkManager.singleton.StartClient();
+            GameController.GameStart();
+        }
+
+        public void FindLocalServer()
+        {
+            if (running) { return; }
+            Initialize();
+            StartAsClient();
+        }
+    }
+}
---
+++ Assets/Scripts/Network/ServerNetworkDiscovery.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace MVP
+{
+    public class ServerNetworkDiscovery : NetworkDiscovery
+    {
+        void Start()
+        {
+            if (running) { return; }
+            Initialize();
+            StartAsServer();
+        }
+    }
+}
